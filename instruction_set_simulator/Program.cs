c7155ec059f4c7d367eded6c919badbc90f48fdf
Jan Poonthong <janpoonthong628@gmail.com>
2025-01-27 09:31:38 +0700

    initialize project

---
+++ instruction_set_simulator/Program.cs
@@ -0,0 +1,9 @@
+﻿class InstructionSetSimulator
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter username: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("Username is: " + userName);
+    }
+}
